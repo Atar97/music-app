0b5609dc31c63f78fcdba06464b469a9836be5f5
Austin <austin.cotant@gmail.com>
2018-09-13 10:13:06 -0700

    Complete auth to user sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,25 @@
 class SessionsController < ApplicationController
-  
+  def new
+    render :new
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+    if @user
+      login!(@user)
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = ["Email and Password don't match"]
+      render :new
+    end
+  end
+
+  def destroy
+    logout!
+    redirect_to new_session_url
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,18 @@
 module ApplicationHelper
+
   def auth_token
     "<input type='hidden' name='authenticity_token' value='#{form_authenticity_token}'/>".html_safe
+  end
+
+  def sign_up_hash(type)
+    result = Hash.new
+    if type == :login
+      result[:path] = session_url
+      result[:button_label] = 'Sign-In'
+    elsif type == :new
+      result[:path] = new_user_url
+      result[:button_label] = 'Sign-Up'
+    end
+    result
   end
 end
