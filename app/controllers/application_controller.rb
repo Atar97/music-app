c1ee876a8767558c19481b9294f8f65b939def82
Austin <austin.cotant@gmail.com>
2018-09-13 10:33:49 -0700

    Debug session#destroy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :loggedin?
+  helper_method :current_user, :logged_in?
 
   def login!(user)
     @current_user = user
-    session[:session_token] = user.reset_session_token!
+    session[:session_token] = user.session_token
   end
 
   def logout!
-    @current_user.reset_session_token!
+    current_user.reset_session_token!
     @current_user = nil
     session[:session_token] = nil
   end
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by(session_token: session[:session_token])
   end
 
-  def loggedin?
+  def logged_in?
     !!current_user
   end
 
