a894dc64a388024d805eb92b64b58058de1bc0d7
Austin <austin.cotant@gmail.com>
2018-09-13 17:34:44 -0700

    Finish track show page

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,12 +1,22 @@
 class TracksController < ApplicationController
-
+  before_action :find_track, only: [:destroy, :show, :edit, :update]
   def show
     render :show
   end
 
   def new
     @track = Track.new
-    @album = Album.find(params[:album_id])
     render :new
   end
+
+  private
+  
+  def track_params
+    params.require(:track).permit(:title, :lyrics, :ord, :regular, :album_id)
+  end
+
+  def find_track
+    @track = Track.find(params[:id])
+  end
+
 end
